4ef1df8e964e77ae4c868d2f2d81d892b90aabb2
KelvinOr <Kelvinor29@gmail.com>
2024-12-03 06:34:59 -0600

    Modal Recreated

--- docs/JS/DisplayCountries.js
+++ docs/JS/DisplayCountries.js
@@ -24,7 +24,6 @@ function RenderCard(countriesList){
     const templateCard = document.getElementById('template-country');
     const countriesContainer = document.getElementById('countries-list');
 
-
     countriesList.forEach(country => {
 
         // Clonación
@@ -36,19 +35,12 @@ function RenderCard(countriesList){
         cardClone.getElementById('country-offName').textContent = country.name.official;
         cardClone.getElementById('country-detail').textContent = country.region;
         
-        const countryCard = cardClone.querySelector('.cardC')
+        const countryCard = cardClone.querySelector('.card')
 
         // Modal
         countryCard.addEventListener('click', () =>{
-            
-            // Modal
-            const modalCountry = new bootstrap.Modal(document.getElementById('modalCard'));
-            const modalTitle = document.getElementById('modalLabel');
-        
-            modalTitle.textContent = `#${country.name.common}`;
-        
-            
-            modalCountry.show();
+
+            ShowModal(country);
 
         });
 
@@ -56,5 +48,23 @@ function RenderCard(countriesList){
 
     });
 }
+
+function ShowModal(country){
+    // Modal
+    const modalCountry = new bootstrap.Modal(document.getElementById('modalCard'));
+
+    const modalTitle = document.getElementById('modalLabel');
+    const countryOffName = document.getElementById('modal-offName');
+    const countryFlag = document.getElementById('modal-flag');
+
+    modalTitle.textContent = country.name.common;
+    countryOffName.textContent = country.name.official;
+    countryFlag.src = country.flags.svg;
+    countryFlag.alt = country.flags.alt || `Bandera del país ${country.name.common}`;
+
+    modalCountry.show();
+
+}
+
 
 DisplayCountries();
